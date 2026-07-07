0af14c01c3cc722df0335635bb97beacdc0417ae
HARI PRASAD L S <06hariumaraja@gmail.com>
2026-07-07 16:16:58 +0530

    Refactor ADXL335 accelerometer code structure
    Refactor code to define pin constants and improve readability.

--- Go Kart Projects/GO_Kart_GY-61__ADXL335_Accelerometer__Project_3/GO_Kart_GY-61__ADXL335_Accelerometer__Project_3.ino
+++ Go Kart Projects/GO_Kart_GY-61__ADXL335_Accelerometer__Project_3/GO_Kart_GY-61__ADXL335_Accelerometer__Project_3.ino
@@ -1,15 +1,36 @@
+// ADXL335 Accelerometer
+// X -> A0
+// Y -> A1
+// Z -> A2
+
+const int xPin = A0;
+const int yPin = A1;
+const int zPin = A2;
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  pinMode(zPin, INPUT);
+
+  Serial.println("ADXL335 Accelerometer");
 }
 
 void loop() {
-  int x = analogRead(A1);
-  int y = analogRead(A2);
-  int z = analogRead(A3);
 
-  Serial.print("X: "); Serial.print(x);
-  Serial.print(" | Y: "); Serial.print(y);
-  Serial.print(" | Z: "); Serial.println(z);
+  int x = analogRead(xPin);
+  int y = analogRead(yPin);
+  int z = analogRead(zPin);
 
-  delay(500);
+  Serial.print("X: ");
+  Serial.print(x);
+
+  Serial.print("   Y: ");
+  Serial.print(y);
+
+  Serial.print("   Z: ");
+  Serial.println(z);
+
+  delay(100);
 }
